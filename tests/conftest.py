0b723d6ee4ca91364f7e268873b6a0b745289b3c
akram09 <akram.boutouchent@canonical.com>
2025-11-21 16:09:00 +0100

    fix: Adding conftest.py for handling substrate

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+from pathlib import Path
+
+import pytest
+from _pytest.config.argparsing import Parser
+
+
+def pytest_addoption(parser: Parser):
+    parser.addoption(
+        "--substrate",
+        action="store",
+        help="Substrate to test, either lxd or microk8s",
+        choices=("lxd", "microk8s"),
+        default="lxd",
+    )
