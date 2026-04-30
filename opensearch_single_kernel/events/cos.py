4587a7fc7f42292dff01decb2adc97ed9d0dad62
Serhii Melnyk <s.melnyk@jazzserve.com>
2026-04-30 23:03:08 +0300

    Address PR comments.

--- opensearch_single_kernel/events/cos.py
+++ opensearch_single_kernel/events/cos.py
@@ -44,10 +44,11 @@ class CosEventsHandler(Object):
         self.charm = charm
 
         if self.charm.substrate == Substrates.VM:
+            # VM
             self.cos_integration = COSAgentProvider(
                 self.charm,
                 relation_name=COS_RELATION,
-                scrape_configs=self.scrape_config,
+                scrape_configs=self.scrape_vm_config,
                 refresh_events=[
                     self.charm.on.config_changed,
                     self.charm.on.set_password_action,
@@ -58,69 +59,46 @@ class CosEventsHandler(Object):
                 metrics_rules_dir="./src/alert_rules/prometheus",
                 log_slots=["opensearch:logs"],
             )
-        else:
-            # 1. Metrics (Prometheus)
-            self.metrics_endpoint = MetricsEndpointProvider(
-                self.charm,
-                relation_name="metrics-endpoint",
-                jobs=self.scrape_config(),
-                refresh_event=[
-                    self.charm.on.config_changed,
-                    self.charm.on.set_password_action,
-                    self.charm.on.secret_changed,
-                    self.charm.on[PEER_RELATION].relation_changed,
-                    self.charm.on[PEER_CLUSTER_RELATION].relation_changed,
-                ],
-                alert_rules_path="./src/alert_rules/prometheus",
-            )
+            return
 
-            # 2. Logs (Loki)
-            self.log_proxy = LogForwarder(
-                self.charm,
-                relation_name="logging",
-            )
+        # K8s
+        # 1. Metrics (Prometheus)
+        self.metrics_endpoint = MetricsEndpointProvider(
+            self.charm,
+            relation_name="metrics-endpoint",
+            jobs=self.scrape_k8s_config(),
+            refresh_event=[
+                self.charm.on.config_changed,
+                self.charm.on.set_password_action,
+                self.charm.on.secret_changed,
+                self.charm.on[PEER_RELATION].relation_changed,
+                self.charm.on[PEER_CLUSTER_RELATION].relation_changed,
+            ],
+            alert_rules_path="./src/alert_rules/prometheus",
+        )
 
-            # 3. Dashboards (Grafana)
-            self.grafana_dashboards = GrafanaDashboardProvider(
-                self.charm, relation_name="grafana-dashboard"
-            )
+        # 2. Logs (Loki)
+        self.log_proxy = LogForwarder(
+            self.charm,
+            relation_name="logging",
+        )
 
-    def scrape_config(self) -> list[dict[str, Any]]:
-        """Generates the scrape config as needed."""
-        if self.charm.substrate == Substrates.VM:
-            if (
-                not (app_secrets := self.charm.state.application.admin_secrets)
-                or not (ca := app_secrets.get("ca-cert"))
-                or not (pwd := self.charm.state.application.cos_password)
-                or not (prometheus_labels := self.charm.cluster_manager.get_prometheus_labels())
-            ):
-                # Not yet ready, waiting for certain values to be set
-                return []
-            return [
-                {
-                    "metrics_path": "/_prometheus/metrics",
-                    "static_configs": [
-                        {
-                            "targets": [f"{self.charm.state.host_ip}:{COS_PORT}"],
-                            "labels": prometheus_labels,
-                        }
-                    ],
-                    "tls_config": {"ca": ca},
-                    "scheme": (
-                        "https" if self.charm.tls_manager.all_tls_resources_stored() else "http"
-                    ),
-                    "basic_auth": {"username": f"{COS_USER}", "password": f"{pwd}"},
-                }
-            ]
-        else:
-            if not (pwd := self.charm.state.application.cos_password) or not (
-                prometheus_labels := self.charm.cluster_manager.get_prometheus_labels()
-            ):
-                # Not yet ready, waiting for certain values to be set
-                return []
+        # 3. Dashboards (Grafana)
+        self.grafana_dashboards = GrafanaDashboardProvider(
+            self.charm, relation_name="grafana-dashboard"
+        )
 
-            tls_enabled = self.charm.tls_manager.all_tls_resources_stored()
-            config: dict[str, Any] = {
+    def scrape_vm_config(self) -> list[dict[str, Any]]:
+        """Generate the scrape config for VM platform."""
+        if (
+            not (ca := self.charm.state.application.admin_secrets.get("ca-cert"))
+            or not (pwd := self.charm.state.application.cos_password)
+            or not (prometheus_labels := self.charm.cluster_manager.get_prometheus_labels())
+        ):
+            # Not yet ready, waiting for certain values to be set
+            return []
+        return [
+            {
                 "metrics_path": "/_prometheus/metrics",
                 "static_configs": [
                     {
@@ -128,9 +106,34 @@ class CosEventsHandler(Object):
                         "labels": prometheus_labels,
                     }
                 ],
-                "scheme": "https" if tls_enabled else "http",
+                "tls_config": {"ca": ca},
+                "scheme": (
+                    "https" if self.charm.tls_manager.all_tls_resources_stored() else "http"
+                ),
                 "basic_auth": {"username": f"{COS_USER}", "password": f"{pwd}"},
             }
-            if tls_enabled:
-                config["tls_config"] = {"insecure_skip_verify": True}
-            return [config]
+        ]
+
+    def scrape_k8s_config(self) -> list[dict[str, Any]]:
+        """Generate the scrape config for K8s platform."""
+        if not (pwd := self.charm.state.application.cos_password) or not (
+            prometheus_labels := self.charm.cluster_manager.get_prometheus_labels()
+        ):
+            # Not yet ready, waiting for certain values to be set
+            return []
+
+        tls_enabled = self.charm.tls_manager.all_tls_resources_stored()
+        config: dict[str, Any] = {
+            "metrics_path": "/_prometheus/metrics",
+            "static_configs": [
+                {
+                    "targets": [f"{self.charm.state.host_ip}:{COS_PORT}"],
+                    "labels": prometheus_labels,
+                }
+            ],
+            "scheme": "https" if tls_enabled else "http",
+            "basic_auth": {"username": f"{COS_USER}", "password": f"{pwd}"},
+        }
+        if tls_enabled:
+            config["tls_config"] = {"insecure_skip_verify": True}
+        return [config]
