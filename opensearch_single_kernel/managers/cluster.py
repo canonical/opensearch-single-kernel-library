dcfe10775912345e49cdce09e8f5b342ab34658c
akram09 <akram.boutouchent@canonical.com>
2026-07-21 17:14:04 +0200

    patch: Address review comment

--- opensearch_single_kernel/managers/cluster.py
+++ opensearch_single_kernel/managers/cluster.py
@@ -963,8 +963,8 @@ class ClusterManager(BaseManager):
             )
 
         if (
-            (config_roles := self.state.config.get("roles"))
-            and isinstance(config_roles, str)
+            (config_roles := list(map(str.strip, self.state.config.get("roles", "").split(","))))
+            and isinstance(config_roles, list)
             and "cluster_manager" in config_roles
             and "voting_only" in config_roles
         ):
